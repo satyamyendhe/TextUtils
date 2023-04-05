beee472b15651bd9756f3fe724d4a225292843bd
satyamyendhe <satyamyendhe7@gmail.com>
2023-04-05 15:00:13 +0530

    Initial Commit

---
+++ src/components/TextForm.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+
+export default function TextForm(props) {
+
+    // useState 
+    const [text, newText] = useState("");
+
+    // convert newtext to Upper Case
+    const handelUpperCase = () => {
+        console.log("UpperCase was clicked");
+        let upperCase = text.toUpperCase()
+        newText(upperCase)
+    }
+    // convert newtext to Lower Case
+    const handelLowerCase = () => {
+        console.log("LowerCase was clicked")
+        let lowerCase = text.toLowerCase();
+        newText(lowerCase);
+    }
+
+    // storing value to newText
+    const handelOnChnage = (event) => {
+        console.log("Text Update")
+        newText(event.target.value);
+    }
+
+    // count the words 
+    let words = text.split(" ").length-1
+
+
+
+    return (
+        <>
+            <div className='container my-5'>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+
+                    {/* value is what you enter in textarea */}
+                    <textarea className="form-control mt-2" value={text} onChange={handelOnChnage} rows="8"></textarea>
+                </div>
+                <button className='btn btn-primary mx-2' onClick={handelUpperCase}>UPPERCASE</button>
+                <button className='btn btn-primary mx-2' onClick={handelLowerCase}>lowercase</button>
+            </div>
+            <div className="container">
+                <h1>Your text Summary</h1>
+                <p> words : {words} <br /> characters : {text.length}</p>
+                <p>{0.008*words} minutes read</p>
+
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+
+        </>
+        )
+
+}
+
+TextForm.propTypes = {
+    heading: PropTypes.string.isRequired
+};
+
+TextForm.defaultProps = {
+    heading: "Enter your heading"
+};
